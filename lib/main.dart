d6ee8d67c02272c7c0dbb99c6e3a64eedcc81231
fauziahmilda <fauziah.milda@gmail.com>
2024-01-11 22:31:02 +0700

    provide an instance of UserProvider to the widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:education_app/core/common/app/providers/user_provider.dart';
 import 'package:education_app/core/res/fonts.dart';
 import 'package:education_app/core/res/my_colors.dart';
 import 'package:education_app/core/services/injection_container.dart';
@@ -5,6 +6,7 @@ import 'package:education_app/core/services/router.dart';
 import 'package:education_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,18 +22,21 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Education App - Zia',
-      theme: ThemeData(
-        useMaterial3: true,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        appBarTheme: const AppBarTheme(color: Colors.transparent),
-        fontFamily: Fonts.poppins,
-        colorScheme: ColorScheme.fromSwatch(
-          accentColor: MyColors.primaryColour,
+    return ChangeNotifierProvider(
+      create: (_) => UserProvider(),
+      child: MaterialApp(
+        title: 'Education App - Zia',
+        theme: ThemeData(
+          useMaterial3: true,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          appBarTheme: const AppBarTheme(color: Colors.transparent),
+          fontFamily: Fonts.poppins,
+          colorScheme: ColorScheme.fromSwatch(
+            accentColor: MyColors.primaryColour,
+          ),
         ),
+        onGenerateRoute: generateRoute,
       ),
-      onGenerateRoute: generateRoute,
     );
   }
 }
