10ef17b5cb1217c0ec7eb27f0b07179f4997521c
fauziahmilda <fauziah.milda@gmail.com>
2024-01-12 00:31:20 +0700

    update sign in screen, haven't done yet

--- lib/src/auth/presentation/views/sign_in_screen.dart
+++ lib/src/auth/presentation/views/sign_in_screen.dart
@@ -1,4 +1,15 @@
+import 'package:education_app/core/common/app/providers/user_provider.dart';
+import 'package:education_app/core/common/widgets/gradient_background.dart';
+import 'package:education_app/core/extensions/context_extension.dart';
+import 'package:education_app/core/res/fonts.dart';
+import 'package:education_app/core/res/media_res.dart';
+import 'package:education_app/core/utils/core_utils.dart';
+import 'package:education_app/src/auth/data/models/user_model.dart';
+import 'package:education_app/src/auth/presentation/bloc/auth_bloc.dart';
+import 'package:education_app/src/auth/presentation/views/sign_up_screen.dart';
+import 'package:education_app/src/dashboard/presentation/views/dashboard.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -10,8 +21,77 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: Colors.white,
+      body: BlocConsumer<AuthBloc, AuthState>(
+        builder: (context, state) {
+          return GradientBackground(
+            image: MediaRes.authGradientBackground,
+            child: ListView(
+              shrinkWrap: true,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              children: [
+                const Text(
+                  'Easy to learn, discover more skills',
+                  style: TextStyle(
+                    fontFamily: Fonts.aeonik,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Sign in to your account',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    Baseline(
+                      baseline: 100,
+                      baselineType: TextBaseline.alphabetic,
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(
+                            context,
+                            SignUpScreen.routeName,
+                          );
+                        },
+                        child: const Text('Register account'),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+              ],
+            ),
+          );
+        },
+        listener: (_, state) {
+          if (state is AuthError) {
+            CoreUtils.showSnackBar(context, state.message);
+          } else if (state is SignedIn) {
+            context
+                .read<UserProvider>()
+                .initUser(state.user as LocalUserModel?);
+            Navigator.pushReplacementNamed(context, Dashboard.routeName);
+          }
+        },
+      ),
+    );
   }
 }
