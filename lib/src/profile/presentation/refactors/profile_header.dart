fd7d3b0f2e4c8b7df456254914517102dc472558
fauziahmilda <fauziah.milda@gmail.com>
2024-01-18 21:40:37 +0700

    profile header has been done

--- lib/src/profile/presentation/refactors/profile_header.dart
+++ lib/src/profile/presentation/refactors/profile_header.dart
@@ -1,5 +1,7 @@
 import 'package:education_app/core/common/app/providers/user_provider.dart';
+import 'package:education_app/core/extensions/context_extension.dart';
 import 'package:education_app/core/res/media_res.dart';
+import 'package:education_app/core/res/my_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -8,6 +10,7 @@ class ProfileHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    debugPrint(context.currentUser!.bio);
     return Consumer<UserProvider>(
       builder: (_, provider, __) {
         final user = provider.user;
@@ -31,6 +34,23 @@ class ProfileHeader extends StatelessWidget {
                 fontWeight: FontWeight.w600,
               ),
             ),
+            if (user?.bio != null && user!.bio!.isNotEmpty) ...[
+              const SizedBox(height: 8),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: context.widht * .15,
+                ),
+                child: Text(
+                  user.bio!,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 12,
+                    color: MyColors.neutralTextColour,
+                  ),
+                ),
+              ),
+            ],
+            const SizedBox(height: 16),
           ],
         );
       },
