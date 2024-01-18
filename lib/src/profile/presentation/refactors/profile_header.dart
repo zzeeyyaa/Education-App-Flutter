41d31c248bff22eecbaa59abb86bc6169b293b15
fauziahmilda <fauziah.milda@gmail.com>
2024-01-18 21:00:25 +0700

    create profile header

---
+++ lib/src/profile/presentation/refactors/profile_header.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProfileHeader extends StatelessWidget {
+  const ProfileHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
