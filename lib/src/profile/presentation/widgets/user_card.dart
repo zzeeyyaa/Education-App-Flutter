2046d2f53de65bd349a7c0d9caf6773356f3fe60
fauziahmilda <fauziah.milda@gmail.com>
2024-01-20 00:35:07 +0700

    create user card

---
+++ lib/src/profile/presentation/widgets/user_card.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class UserCard extends StatelessWidget {
+  const UserCard({
+    required this.infoThemeColor,
+    required this.infoIcon,
+    required this.infoTitle,
+    required this.infoValue,
+    super.key,
+  });
+
+  final Color infoThemeColor;
+  final Widget infoIcon;
+  final String infoTitle;
+  final String infoValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 84,
+      width: 156,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade400),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 40,
+              width: 40,
+              decoration: const BoxDecoration(
+                color: Colors.red,
+                shape: BoxShape.circle,
+              ),
+              child: Center(
+                child: Icon(Icons.bubble_chart),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  'infoTitle',
+                  style: const TextStyle(fontSize: 12),
+                ),
+                Text(
+                  'infoValue',
+                  style: const TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
