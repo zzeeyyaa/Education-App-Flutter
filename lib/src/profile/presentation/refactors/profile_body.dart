90f557d6efd39d01e59fda5da6b048c73d464c4d
fauziahmilda <fauziah.milda@gmail.com>
2024-01-20 23:44:36 +0700

    profile body has been done

--- lib/src/profile/presentation/refactors/profile_body.dart
+++ lib/src/profile/presentation/refactors/profile_body.dart
@@ -1,10 +1,79 @@
+import 'package:education_app/core/common/app/providers/user_provider.dart';
+import 'package:education_app/core/res/media_res.dart';
+import 'package:education_app/core/res/my_colors.dart';
+import 'package:education_app/src/profile/presentation/widgets/user_card.dart';
 import 'package:flutter/material.dart';
+import 'package:iconly/iconly.dart';
+import 'package:provider/provider.dart';
 
 class ProfileBody extends StatelessWidget {
   const ProfileBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer<UserProvider>(
+      builder: (_, provider, __) {
+        final user = provider.user;
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: UserCard(
+                    infoThemeColor: MyColors.physicsTileColour,
+                    infoIcon: const Icon(IconlyLight.document),
+                    infoTitle: 'Course',
+                    infoValue: user!.enrolledCourseIds.length.toString(),
+                  ),
+                ),
+                const SizedBox(width: 20),
+                Expanded(
+                  child: UserCard(
+                    infoThemeColor: MyColors.languageTileColour,
+                    infoIcon: Image.asset(
+                      MediaRes.scoreboard,
+                      height: 24,
+                    ),
+                    infoTitle: 'Score',
+                    infoValue: user.points.toString(),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(width: 20),
+            Row(
+              children: [
+                Expanded(
+                  child: UserCard(
+                    infoThemeColor: MyColors.biologyTileColour,
+                    infoIcon: const Icon(
+                      IconlyLight.user,
+                      color: Color(0xFF56AEFF),
+                      size: 24,
+                    ),
+                    infoTitle: 'Followers',
+                    infoValue: user.followers.length.toString(),
+                  ),
+                ),
+                const SizedBox(width: 20),
+                Expanded(
+                  child: UserCard(
+                    infoThemeColor: MyColors.chemistryTileColour,
+                    infoIcon: const Icon(
+                      IconlyLight.user,
+                      color: Color(0xFFFF84AA),
+                      size: 24,
+                    ),
+                    infoTitle: 'Following',
+                    infoValue: user.followers.length.toString(),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
   }
 }
