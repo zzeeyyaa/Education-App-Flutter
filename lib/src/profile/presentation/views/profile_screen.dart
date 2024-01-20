e780ee500c8694f9c517936a5cce6c2bb0b3cef2
fauziahmilda <fauziah.milda@gmail.com>
2024-01-20 23:45:22 +0700

    profile screen has been done by adding profile body and profile header

--- lib/src/profile/presentation/views/profile_screen.dart
+++ lib/src/profile/presentation/views/profile_screen.dart
@@ -1,3 +1,8 @@
+import 'package:education_app/core/common/widgets/gradient_background.dart';
+import 'package:education_app/core/res/media_res.dart';
+import 'package:education_app/src/profile/presentation/refactors/profile_body.dart';
+import 'package:education_app/src/profile/presentation/refactors/profile_header.dart';
+import 'package:education_app/src/profile/presentation/widgets/profile_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class ProfileView extends StatelessWidget {
@@ -5,6 +10,20 @@ class ProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: Colors.white,
+      appBar: const ProfileAppBar(),
+      body: GradientBackground(
+        image: MediaRes.profileGradientBackground,
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          children: const [
+            ProfileHeader(),
+            ProfileBody(),
+          ],
+        ),
+      ),
+    );
   }
 }
