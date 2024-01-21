fe8d98b80e900aec0e5109b693277d1cbcbab566
fauziahmilda <fauziah.milda@gmail.com>
2024-01-21 18:29:56 +0700

    has finished preparing the req for data changes

--- lib/src/profile/presentation/views/edit_profile_view.dart
+++ lib/src/profile/presentation/views/edit_profile_view.dart
@@ -1,6 +1,7 @@
-import 'package:education_app/core/common/app/providers/user_provider.dart';
+import 'dart:io';
+import 'package:education_app/core/extensions/context_extension.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:image_picker/image_picker.dart';
 
 class EditProfileView extends StatefulWidget {
   const EditProfileView({super.key});
@@ -10,10 +11,67 @@ class EditProfileView extends StatefulWidget {
 }
 
 class _EditProfileViewState extends State<EditProfileView> {
+  //actively checking changes
+  //save or collect the info of currenet user
+
+  final emailController = TextEditingController();
+  final fullNameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final oldPasswordController = TextEditingController();
+  final bioController = TextEditingController();
+
+  File? pickedImage;
+
+  Future<void> pickImage() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState(() {
+        pickedImage = File(image.path);
+      });
+    }
+  }
+
+  bool get nameChanged =>
+      context.currentUser!.fullName.trim() != fullNameController.text.trim();
+
+  bool get emailChanged =>
+      context.currentUser!.email.trim() != emailController.text.trim();
+
+  bool get passwordChanged => passwordController.text.trim().isNotEmpty;
+
+  bool get bioChanged =>
+      context.currentUser?.bio?.trim() != bioController.text.trim();
+
+  bool get imageChange => pickedImage != null;
+
+  bool get nothingChanged =>
+      !nameChanged &&
+      !emailChanged &&
+      !passwordChanged &&
+      !bioChanged &&
+      !imageChange;
+
+  @override
+  void initState() {
+    fullNameController.text = context.currentUser!.fullName.trim();
+    emailController.text = context.currentUser!.email.trim();
+    bioController.text = context.currentUser!.bio?.trim() ?? '';
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    fullNameController.dispose();
+    emailController.dispose();
+    bioController.dispose();
+    passwordController.dispose();
+    oldPasswordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<UserProvider>(
-      builder: (context, provider, child) {},
-    );
+    return const Placeholder();
   }
 }
