e8caa983d819b71ae5fc5728ff81a5dc3ff57770
fauziahmilda <fauziah.milda@gmail.com>
2024-01-23 08:14:56 +0700

    edit profile from is done

--- lib/src/profile/presentation/widgets/edit_profile_form.dart
+++ lib/src/profile/presentation/widgets/edit_profile_form.dart
@@ -1,3 +1,6 @@
+import 'package:education_app/core/extensions/context_extension.dart';
+import 'package:education_app/core/extensions/string_extension.dart';
+import 'package:education_app/src/profile/presentation/widgets/edit_profile_form_field.dart';
 import 'package:flutter/material.dart';
 
 class EditProfileForm extends StatelessWidget {
@@ -18,6 +21,41 @@ class EditProfileForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        EditProfileFormField(
+          fieldTitle: 'FULL NAME',
+          controller: fullNameController,
+          hintText: context.currentUser!.fullName,
+        ),
+        EditProfileFormField(
+          fieldTitle: 'EMAIL',
+          controller: emailController,
+          hintText: context.currentUser!.email.obscureEmail,
+        ),
+        EditProfileFormField(
+          fieldTitle: 'CURRENT PASSWORD',
+          controller: oldPasswordController,
+          hintText: '*******',
+        ),
+        StatefulBuilder(
+          builder: (_, setState) {
+            oldPasswordController.addListener(() => setState(() {}));
+            return EditProfileFormField(
+              fieldTitle: 'NEW PASSWORD',
+              controller: passwordController,
+              hintText: '*******',
+              readOnly: oldPasswordController.text.isEmpty,
+            );
+          },
+        ),
+        EditProfileFormField(
+          fieldTitle: 'BIO',
+          controller: bioController,
+          hintText: context.currentUser!.bio,
+        ),
+      ],
+    );
   }
 }
