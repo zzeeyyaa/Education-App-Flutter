c84bf7affc15158493c64cf68aa79a0a3b7b8f3f
fauziahmilda <fauziah.milda@gmail.com>
2024-01-23 14:42:08 +0700

    create course repo

---
+++ lib/core/common/features/course/domain/repos/course_repo.dart
@@ -0,0 +1,10 @@
+import 'package:education_app/core/common/features/course/domain/entities/course.dart';
+import 'package:education_app/core/utils/typedefs.dart';
+
+abstract class CourseRepo {
+  const CourseRepo();
+
+  ResultFuture<List<Course>> getCourses();
+
+  ResultVoid addCourse(Course course);
+}
