32926822999d739df76f678a53179b5a05388126
fauziahmilda <fauziah.milda@gmail.com>
2024-01-23 14:37:54 +0700

    create course entities on domain layer, created it on core - common

---
+++ lib/core/common/features/course/domain/entities/course.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+class Course extends Equatable {
+  const Course({
+    required this.id,
+    required this.title,
+    required this.numberOfExams,
+    required this.numberOfMaterials,
+    required this.numberOfVideos,
+    required this.groupId,
+    required this.createdAt,
+    required this.updatedAt,
+    this.description,
+    this.image,
+    this.imageIsFile = false,
+  });
+
+  final String id;
+  final String title;
+  final String? description;
+  final int numberOfExams;
+  final int numberOfMaterials;
+  final int numberOfVideos;
+  final String groupId;
+  final String? image;
+  final bool imageIsFile;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  @override
+  List<Object?> get props => [id];
+}
