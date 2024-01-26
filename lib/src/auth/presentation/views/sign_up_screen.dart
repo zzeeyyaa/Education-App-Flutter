4cb63fc7ce686dcbd547e5920a73bef7399d9754
fauziahmilda <fauziah.milda@gmail.com>
2024-01-26 21:24:18 +0700

    sign up screen update

--- lib/src/auth/presentation/views/sign_up_screen.dart
+++ lib/src/auth/presentation/views/sign_up_screen.dart
@@ -9,13 +9,14 @@ import 'package:education_app/src/auth/presentation/bloc/auth_bloc.dart';
 import 'package:education_app/src/auth/presentation/views/sign_in_screen.dart';
 import 'package:education_app/src/auth/presentation/widgets/sign_up_form.dart';
 import 'package:education_app/src/dashboard/presentation/views/dashboard.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
 
-  static const routeName = 'sign-up';
+  static const routeName = '/sign-up';
 
   @override
   State<SignUpScreen> createState() => _SignUpScreenState();
@@ -42,7 +43,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: BlocConsumer<AuthBloc, AuthState>(
-        listener: (context, state) {
+        listener: (_, state) {
           if (state is AuthError) {
             CoreUtils.showSnackBar(context, state.message);
           } else if (state is SignedUp) {
@@ -60,68 +61,68 @@ class _SignUpScreenState extends State<SignUpScreen> {
         builder: (context, state) {
           return GradientBackground(
             image: MediaRes.authGradientBackground,
-            child: Center(
-              child: ListView(
-                shrinkWrap: true,
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                children: [
-                  const Text(
-                    'Easy to learn, discover more skills',
-                    style: TextStyle(
-                      fontFamily: Fonts.aeonik,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 32,
+            child: SafeArea(
+              child: Center(
+                child: ListView(
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  children: [
+                    const Text(
+                      'Easy to learn, discover more skills.',
+                      style: TextStyle(
+                        fontFamily: Fonts.aeonik,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 32,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Sign up to create your account',
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Sign up for an account',
                       style: TextStyle(fontSize: 14),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.pushReplacementNamed(
-                          context,
-                          SignInScreen.routeName,
-                        );
-                      },
-                      child: const Text('Already have an account?'),
+                    const SizedBox(height: 10),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(
+                            context,
+                            SignInScreen.routeName,
+                          );
+                        },
+                        child: const Text('Already have an account?'),
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  SignUpForm(
-                    emailController: emailController,
-                    fullNameController: fullNameController,
-                    passwordController: passwordController,
-                    confirmPasswordController: confirmPasswordController,
-                    formKey: formKey,
-                  ),
-                  const SizedBox(height: 30),
-                  if (state is AuthLoading)
-                    const Center(child: CircularProgressIndicator())
-                  else
-                    RoundedButton(
-                      label: 'Sign Up',
-                      onPressed: () {
-                        FocusManager.instance.primaryFocus?.unfocus();
-                        if (formKey.currentState!.validate()) {
-                          context.read<AuthBloc>().add(
-                                SignUpEvent(
-                                  email: emailController.text.trim(),
-                                  password: passwordController.text.trim(),
-                                  fullName: fullNameController.text.trim(),
-                                ),
-                              );
-                        }
-                      },
+                    const SizedBox(height: 10),
+                    SignUpForm(
+                      emailController: emailController,
+                      fullNameController: fullNameController,
+                      passwordController: passwordController,
+                      confirmPasswordController: confirmPasswordController,
+                      formKey: formKey,
                     ),
-                ],
+                    const SizedBox(height: 30),
+                    if (state is AuthLoading)
+                      const Center(child: CircularProgressIndicator())
+                    else
+                      RoundedButton(
+                        label: 'Sign Up',
+                        onPressed: () {
+                          FocusManager.instance.primaryFocus?.unfocus();
+                          FirebaseAuth.instance.currentUser?.reload();
+                          if (formKey.currentState!.validate()) {
+                            context.read<AuthBloc>().add(
+                                  SignUpEvent(
+                                    email: emailController.text.trim(),
+                                    password: passwordController.text.trim(),
+                                    fullName: fullNameController.text.trim(),
+                                  ),
+                                );
+                          }
+                        },
+                      ),
+                  ],
+                ),
               ),
             ),
           );
