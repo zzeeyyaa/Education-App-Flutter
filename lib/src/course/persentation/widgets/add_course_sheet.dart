86f61e3d884138c69c03564b9a5e08bab33b5168
fauziahmilda <fauziah.milda@gmail.com>
2024-01-27 20:56:26 +0700

    create add course sheet

---
+++ lib/src/course/persentation/widgets/add_course_sheet.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddCourseSheet extends StatefulWidget {
+  const AddCourseSheet({super.key});
+
+  @override
+  State<AddCourseSheet> createState() => _AddCourseSheetState();
+}
+
+class _AddCourseSheetState extends State<AddCourseSheet> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
