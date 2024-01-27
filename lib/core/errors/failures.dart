cdc235d2c792f7a561b628391e42d54bf7df6195
fauziahmilda <fauziah.milda@gmail.com>
2024-01-27 16:45:21 +0700

    uodate failure get errorMessage

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -11,8 +11,11 @@ abstract class Failure extends Equatable {
   final String message;
   final dynamic statusCode;
 
-  String get errorMeesage =>
-      '$statusCode ${statusCode is String ? '' : 'Error'} : $message';
+  String get errorMeesage {
+    final showErrorText =
+        statusCode! is String || int.tryParse(statusCode as String) != null;
+    return '$statusCode ${showErrorText ? 'Error' : ''} : $message';
+  }
 
   @override
   List<dynamic> get props => [message, statusCode];
