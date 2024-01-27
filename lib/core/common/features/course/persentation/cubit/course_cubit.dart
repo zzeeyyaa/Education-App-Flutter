d1142b1760eb5df3136377459c3e8f8cea34c076
fauziahmilda <fauziah.milda@gmail.com>
2024-01-27 15:52:37 +0700

    create cubit for course

---
+++ lib/core/common/features/course/persentation/cubit/course_cubit.dart
@@ -0,0 +1,32 @@
+import 'package:bloc/bloc.dart';
+import 'package:education_app/core/common/features/course/domain/entities/course.dart';
+import 'package:education_app/core/common/features/course/domain/usecases/add_course.dart';
+import 'package:education_app/core/common/features/course/domain/usecases/get_courses.dart';
+import 'package:equatable/equatable.dart';
+
+part 'course_state.dart';
+
+class CourseCubit extends Cubit<CourseState> {
+  CourseCubit(this._addCourse, this._getCourse) : super(const CourseInitial());
+
+  final AddCourse _addCourse;
+  final GetCourse _getCourse;
+
+  Future<void> addCourse(Course course) async {
+    emit(const AddingCourse());
+    final result = await _addCourse(course);
+    result.fold(
+      (failure) => emit(CourseError(failure.errorMeesage)),
+      (_) => emit(const CourseAdded()),
+    );
+  }
+
+  Future<void> getCourse() async {
+    emit(const LoadingCourses());
+    final result = await _getCourse();
+    result.fold(
+      (failure) => emit(CourseError(failure.errorMeesage)),
+      (courses) => emit(CoursesLoaded(courses)),
+    );
+  }
+}
