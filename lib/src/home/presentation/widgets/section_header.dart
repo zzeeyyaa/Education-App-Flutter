7ad706a8d426ac76521e31291a197cad473d6bb4
fauziahmilda <fauziah.milda@gmail.com>
2024-01-29 19:28:22 +0700

    ops

--- lib/src/home/presentation/widgets/section_header.dart
+++ lib/src/home/presentation/widgets/section_header.dart
@@ -1,7 +1,5 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:flutter/material.dart';
-
 import 'package:education_app/core/res/my_colors.dart';
+import 'package:flutter/material.dart';
 
 class SectionHeader extends StatelessWidget {
   const SectionHeader({
