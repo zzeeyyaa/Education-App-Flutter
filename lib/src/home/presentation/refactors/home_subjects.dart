192ee282d95f04bb5c05975d2927bd114e101117
fauziahmilda <fauziah.milda@gmail.com>
2024-01-30 00:07:25 +0700

    update home subjects: to show courses data

--- lib/src/home/presentation/refactors/home_subjects.dart
+++ lib/src/home/presentation/refactors/home_subjects.dart
@@ -1,5 +1,8 @@
+import 'package:education_app/core/common/widgets/course_tile.dart';
 import 'package:education_app/core/extensions/context_extension.dart';
+import 'package:education_app/core/res/my_colors.dart';
 import 'package:education_app/src/course/domain/entities/course.dart';
+import 'package:education_app/src/course/persentation/views/all_courses_view.dart';
 import 'package:education_app/src/home/presentation/widgets/section_header.dart';
 import 'package:flutter/material.dart';
 
@@ -17,8 +20,31 @@ class HomeSubjects extends StatelessWidget {
         SectionHeader(
           sectionTitle: 'Courses',
           seeAll: courses.length > 4,
-          onSeeAll: () => context.push(const Placeholder()),
+          onSeeAll: () => context.push(AllCoursesView(courses: courses)),
         ),
+        const Text(
+          'Explore our courses',
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            color: MyColors.neutralTextColour,
+          ),
+        ),
+        const SizedBox(height: 20),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: courses
+              .take(4)
+              .map(
+                (course) => CourseTile(
+                  course: course,
+                  //TODO(COURSE DETAILS) : change to pushNamed and route
+                  onTap: () => Navigator.of(context)
+                      .pushNamed('/unknown-route', arguments: course),
+                ),
+              )
+              .toList(),
+        )
       ],
     );
   }
