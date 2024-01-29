24dbf1d3bca5cf546135525beb6b829cecd92399
fauziahmilda <fauziah.milda@gmail.com>
2024-01-29 19:27:54 +0700

    ops

--- lib/src/home/presentation/refactors/home_subjects.dart
+++ lib/src/home/presentation/refactors/home_subjects.dart
@@ -1,4 +1,6 @@
+import 'package:education_app/core/extensions/context_extension.dart';
 import 'package:education_app/src/course/domain/entities/course.dart';
+import 'package:education_app/src/home/presentation/widgets/section_header.dart';
 import 'package:flutter/material.dart';
 
 class HomeSubjects extends StatelessWidget {
@@ -8,6 +10,16 @@ class HomeSubjects extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SectionHeader(
+          sectionTitle: 'Courses',
+          seeAll: courses.length > 4,
+          onSeeAll: () => context.push(const Placeholder()),
+        ),
+      ],
+    );
   }
 }
