1b6d32d1fdffd6791dcb151e3110bfc6fd90c5eb
fauziahmilda <fauziah.milda@gmail.com>
2024-01-29 18:04:36 +0700

    add tinder card

--- lib/src/home/presentation/refactors/home_header.dart
+++ lib/src/home/presentation/refactors/home_header.dart
@@ -1,4 +1,6 @@
 import 'package:education_app/core/common/app/providers/user_provider.dart';
+import 'package:education_app/core/extensions/context_extension.dart';
+import 'package:education_app/src/home/presentation/widgets/tinder_cards.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,8 +15,22 @@ class HomeHeader extends StatelessWidget {
         children: [
           Text(
             'Hello\n${context.watch<UserProvider>().user!.fullName}',
-            style: const TextStyle(),
-          )
+            style: const TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 36,
+            ),
+          ),
+          Positioned(
+            top: context.height >= 926
+                ? -25
+                : context.height >= 844
+                    ? -6
+                    : context.height <= 800
+                        ? 10
+                        : 10,
+            right: -14,
+            child: const TinderCards(),
+          ),
         ],
       ),
     );
