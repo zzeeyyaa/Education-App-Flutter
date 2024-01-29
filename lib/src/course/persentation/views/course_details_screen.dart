756e72d26e6f7977c6a6d8d97db3a64b24a174b9
fauziahmilda <fauziah.milda@gmail.com>
2024-01-30 00:03:10 +0700

    creating course detail screen

---
+++ lib/src/course/persentation/views/course_details_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CourseDetailScreen extends StatelessWidget {
+  const CourseDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
