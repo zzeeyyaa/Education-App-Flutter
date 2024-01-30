48d061371da5e67531f0dde5e9e3ebd9b7784dc9
fauziahmilda <fauziah.milda@gmail.com>
2024-01-30 17:44:10 +0700

    expandable text has been done

--- lib/core/common/widgets/expandable_text.dart
+++ lib/core/common/widgets/expandable_text.dart
@@ -1,13 +1,18 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:education_app/core/extensions/context_extension.dart';
+import 'package:education_app/core/res/my_colors.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class ExpandableText extends StatefulWidget {
-  const ExpandableText({
+  const ExpandableText(
+    this.context, {
     required this.text,
     super.key,
     this.style,
   });
 
+  final BuildContext context;
   final String text;
   final TextStyle? style;
 
@@ -21,7 +26,72 @@ class _ExpandableTextState extends State<ExpandableText> {
   late TextPainter textPainter;
 
   @override
+  void initState() {
+    textSpan = TextSpan(
+      text: widget.text,
+    );
+
+    textPainter = TextPainter(
+      text: textSpan,
+      textDirection: TextDirection.ltr,
+      maxLines: expanded ? null : 2,
+    )..layout(maxWidth: widget.context.width * .9);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    textPainter.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    const defaultStyle = TextStyle(
+      height: 1.8,
+      fontSize: 16,
+      color: MyColors.neutralTextColour,
+    );
+    return Container(
+      child: textPainter.didExceedMaxLines
+          ? RichText(
+              text: TextSpan(
+                text: expanded
+                    ? widget.text
+                    : '${widget.text.substring(
+                        0,
+                        textPainter
+                            .getPositionForOffset(
+                              Offset(
+                                widget.context.width,
+                                widget.context.height,
+                              ),
+                            )
+                            .offset,
+                      )}...',
+                style: widget.style ?? defaultStyle,
+                children: [
+                  TextSpan(
+                    text: expanded ? 'Show less' : 'Show more',
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        setState(() {
+                          expanded = !expanded;
+                        });
+                      },
+                    style: const TextStyle(
+                      color: MyColors.primaryColour,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          : Text(
+              widget.text,
+              style: widget.style ?? defaultStyle,
+            ),
+    );
   }
 }
