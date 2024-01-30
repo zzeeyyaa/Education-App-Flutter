f076dd392602f8a4d1af5506ea2e45d438fe5021
fauziahmilda <fauziah.milda@gmail.com>
2024-01-30 20:39:59 +0700

    fix typo

--- lib/src/course/features/videos/domain/usecases/get_videos.dart
+++ lib/src/course/features/videos/domain/usecases/get_videos.dart
@@ -3,8 +3,8 @@ import 'package:education_app/core/utils/typedefs.dart';
 import 'package:education_app/src/course/features/videos/domain/entities/video.dart';
 import 'package:education_app/src/course/features/videos/domain/repos/video_repo.dart';
 
-class GetVideo extends UsecaseWithParams<List<Video>, String> {
-  const GetVideo(this._repo);
+class GetVideos extends UsecaseWithParams<List<Video>, String> {
+  const GetVideos(this._repo);
 
   final VideoRepo _repo;
 
