3cc0e72a41002c746e76bed10e05e2a1b6f04bf6
fauziahmilda <fauziah.milda@gmail.com>
2024-01-30 18:59:16 +0700

    course info tile - done

--- lib/core/common/widgets/course_info_tile.dart
+++ lib/core/common/widgets/course_info_tile.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:education_app/core/res/my_colors.dart';
 import 'package:flutter/material.dart';
 
 class CourseInfoTile extends StatelessWidget {
@@ -19,9 +20,38 @@ class CourseInfoTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Row(children: [
-        SizedBox(),
-      ]),
+      child: Row(
+        children: [
+          SizedBox(
+            height: 48,
+            width: 48,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 3),
+              child: Transform.scale(
+                scale: 1.45,
+                child: Image.asset(image),
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(fontWeight: FontWeight.w500),
+              ),
+              Text(
+                subtitle,
+                style: const TextStyle(
+                  color: MyColors.neutralTextColour,
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
