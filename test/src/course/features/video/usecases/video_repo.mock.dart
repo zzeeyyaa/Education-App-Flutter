6e39715011c2c278d60ff43103ad6e63552f129c
fauziahmilda <fauziah.milda@gmail.com>
2024-01-30 22:05:26 +0700

    ops

--- test/src/course/features/video/usecases/video_repo.mock.dart
+++
@@ -1,4 +0,0 @@
-import 'package:education_app/src/course/features/videos/domain/repos/video_repo.dart';
-import 'package:mocktail/mocktail.dart';
-
-class MockVideoRepo extends Mock implements VideoRepo {}
