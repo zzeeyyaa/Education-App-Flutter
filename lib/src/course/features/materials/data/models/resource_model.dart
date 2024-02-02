ddf9d5390023ce675877d9cfb4e03e83d961d985
fauziahmilda <fauziah.milda@gmail.com>
2024-02-02 23:16:17 +0700

    create resource model for materials data - models

---
+++ lib/src/course/features/materials/data/models/resource_model.dart
@@ -0,0 +1,77 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:education_app/core/utils/typedefs.dart';
+import 'package:education_app/src/course/features/materials/domain/entities/resource.dart';
+
+class ResourceModel extends Resource {
+  const ResourceModel({
+    required super.id,
+    required super.courseId,
+    required super.uploadedDate,
+    required super.fileURL,
+    required super.isFile,
+    required super.fileExtension,
+    super.title,
+    super.author,
+    super.description,
+  });
+
+  ResourceModel.empty([DateTime? date])
+      : this(
+          id: '',
+          title: '',
+          description: '',
+          uploadedDate: date ?? DateTime.now(),
+          fileExtension: '',
+          isFile: true,
+          courseId: '',
+          fileURL: '',
+          author: '',
+        );
+  ResourceModel.fromMap(DataMap map)
+      : super(
+          id: map['id'] as String,
+          title: map['title'] as String?,
+          description: map['description'] as String?,
+          uploadedDate: (map['uploadedDate'] as Timestamp).toDate(),
+          fileExtension: map['fileExtension'] as String,
+          isFile: map['isFile'] as bool,
+          courseId: map['courseId'] as String,
+          fileURL: map['fileURL'] as String,
+          author: map['author'] as String?,
+        );
+
+  ResourceModel copyWith({
+    String? id,
+    String? courseId,
+    DateTime? uploadedDate,
+    String? fileURL,
+    bool? isFile,
+    String? fileExtension,
+    String? title,
+    String? author,
+    String? description,
+  }) {
+    return ResourceModel(
+      id: id ?? this.id,
+      courseId: courseId ?? this.courseId,
+      uploadedDate: uploadedDate ?? this.uploadedDate,
+      fileURL: fileURL ?? this.fileURL,
+      isFile: isFile ?? this.isFile,
+      fileExtension: fileExtension ?? this.fileExtension,
+    );
+  }
+
+  DataMap toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'courseId': courseId,
+      'description': description,
+      'uploadedDate': uploadedDate,
+      'isFile': isFile,
+      'fileURL': fileURL,
+      'fileExtension': fileExtension,
+      'author': author,
+    };
+  }
+}
