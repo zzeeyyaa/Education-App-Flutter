3ccb38c622896bd4621f570dbbbe799f0f7debc0
fauziahmilda <fauziah.milda@gmail.com>
2024-02-05 10:36:26 +0700

    create usecases: add material and get materials

---
+++ lib/src/course/features/materials/domain/usecases/add_material.dart
@@ -0,0 +1,15 @@
+import 'package:education_app/core/usecase/usecase.dart';
+import 'package:education_app/core/utils/typedefs.dart';
+import 'package:education_app/src/course/features/materials/domain/entities/resource.dart';
+import 'package:education_app/src/course/features/materials/domain/repos/material_repo.dart';
+
+class AddMaterial extends UsecaseWithParams<void, Resource> {
+  const AddMaterial(this._materialRepo);
+
+  final MaterialRepo _materialRepo;
+
+  @override
+  ResultFuture<void> call(Resource params) {
+    return _materialRepo.addMaterial(params);
+  }
+}
---
+++ lib/src/course/features/materials/domain/usecases/get_materials.dart
@@ -0,0 +1,15 @@
+import 'package:education_app/core/usecase/usecase.dart';
+import 'package:education_app/core/utils/typedefs.dart';
+import 'package:education_app/src/course/features/materials/domain/entities/resource.dart';
+import 'package:education_app/src/course/features/materials/domain/repos/material_repo.dart';
+
+class GetMaterials extends UsecaseWithParams<List<Resource>, String> {
+  const GetMaterials(this._materialRepo);
+
+  final MaterialRepo _materialRepo;
+
+  @override
+  ResultFuture<List<Resource>> call(String params) {
+    return _materialRepo.getMaterials(params);
+  }
+}
