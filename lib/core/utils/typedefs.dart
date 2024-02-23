aa5579bbc5725bcc9c93afb71a83b765ea17756d
fauziahmilda <fauziah.milda@gmail.com>
2024-02-23 11:15:09 +0700

    update typedef by adding ResultStream

--- lib/core/utils/typedefs.dart
+++ lib/core/utils/typedefs.dart
@@ -5,4 +5,6 @@ typedef ResultFuture<T> = Future<Either<Failure, T>>;
 
 typedef ResultVoid = ResultFuture<void>;
 
+typedef ResultStream<T> = Stream<Either<Failure, T>>;
+
 typedef DataMap = Map<String, dynamic>;
