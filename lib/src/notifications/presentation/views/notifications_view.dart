1a17ebd04d2355641c0f858991901cd5c3701038
fauziahmilda <fauziah.milda@gmail.com>
2024-02-27 12:33:23 +0700

    fix unused lib

--- lib/src/notifications/presentation/views/notifications_view.dart
+++ lib/src/notifications/presentation/views/notifications_view.dart
@@ -9,7 +9,6 @@ import 'package:education_app/src/notifications/presentation/widgets/notificatio
 import 'package:education_app/src/notifications/presentation/widgets/notification_tile.dart';
 import 'package:flutter/material.dart' hide Badge;
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
 
 class NotificationsView extends StatefulWidget {
   const NotificationsView({super.key});
