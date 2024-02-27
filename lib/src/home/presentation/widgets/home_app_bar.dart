bc6f7018f34c70b97ce6d7a19dbae077e7ae8da4
fauziahmilda <fauziah.milda@gmail.com>
2024-02-27 12:36:09 +0700

    fix unused lib

--- lib/src/home/presentation/widgets/home_app_bar.dart
+++ lib/src/home/presentation/widgets/home_app_bar.dart
@@ -2,7 +2,6 @@ import 'package:education_app/core/common/app/providers/user_provider.dart';
 import 'package:education_app/core/res/media_res.dart';
 import 'package:education_app/src/home/presentation/widgets/notification_bell.dart';
 import 'package:flutter/material.dart';
-import 'package:iconly/iconly.dart';
 import 'package:provider/provider.dart';
 
 class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
