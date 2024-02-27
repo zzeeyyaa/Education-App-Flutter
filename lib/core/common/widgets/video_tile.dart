32afd499f96021c50a4be4f34f1d08dbe3cad45f
fauziahmilda <fauziah.milda@gmail.com>
2024-02-27 21:48:58 +0700

    creating video tile core widget

---
+++ lib/core/common/widgets/video_tile.dart
@@ -0,0 +1,80 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:io';
+
+import 'package:education_app/core/extensions/string_extension.dart';
+import 'package:education_app/core/res/media_res.dart';
+import 'package:education_app/src/course/features/videos/data/models/video_model.dart';
+import 'package:education_app/src/course/features/videos/domain/entities/video.dart';
+import 'package:education_app/src/course/features/videos/presentation/utils/video_utils.dart';
+import 'package:flutter/material.dart';
+
+class VideoTile extends StatelessWidget {
+  const VideoTile(
+    this.video, {
+    super.key,
+    this.tappable = false,
+    this.uploadTimePrefix = 'Uploaded',
+    this.isFile = false,
+  });
+
+  final Video video;
+  final bool tappable;
+  final bool isFile;
+  final String uploadTimePrefix;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      height: 108,
+      child: Row(
+        children: [
+          GestureDetector(
+            onTap: tappable
+                ? () => VideoUtils.playVideo(context, video.videoURL)
+                : null,
+            child: Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  height: 108,
+                  width: 130,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: video.thumbnail == null
+                          ? const AssetImage(MediaRes.thumbnailPlaceholder)
+                          : isFile
+                              ? FileImage(File(video.thumbnail!))
+                              : NetworkImage(video.thumbnail!) as ImageProvider,
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                ),
+                if (tappable)
+                  Container(
+                    height: 108,
+                    width: 130,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(24),
+                      color: Colors.black.withOpacity(.3),
+                    ),
+                    child: Center(
+                      child: video.videoURL.isYoutubeVideo
+                          ? Image.asset(MediaRes.youtube, height: 40)
+                          : const Icon(
+                              Icons.play_arrow_rounded,
+                              color: Colors.white,
+                              size: 40,
+                            ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 20),
+        ],
+      ),
+    );
+  }
+}
