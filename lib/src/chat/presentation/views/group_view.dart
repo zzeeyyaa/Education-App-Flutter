c2f62fe730f09d37fb27084cf6070d0d09426897
fauziahmilda <fauziah.milda@gmail.com>
2024-03-04 10:46:42 +0700

    group view done

--- lib/src/chat/presentation/views/group_view.dart
+++ lib/src/chat/presentation/views/group_view.dart
@@ -4,6 +4,8 @@ import 'package:education_app/core/extensions/context_extension.dart';
 import 'package:education_app/core/utils/core_utils.dart';
 import 'package:education_app/src/chat/domain/entities/group.dart';
 import 'package:education_app/src/chat/presentation/cubit/chat_cubit.dart';
+import 'package:education_app/src/chat/presentation/widgets/other_group_tile.dart';
+import 'package:education_app/src/chat/presentation/widgets/your_group_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -70,8 +72,36 @@ class _GroupsViewState extends State<GroupsView> {
               'No groups found\nPlease contect admin or if you are admin'
               ' add courses',
             );
+          } else if ((state is GroupsLoaded) ||
+              (yourGroups.isNotEmpty) ||
+              (otherGroups.isNotEmpty)) {
+            return ListView(
+              padding: const EdgeInsets.all(20),
+              children: [
+                if (yourGroups.isNotEmpty) ...[
+                  Text(
+                    'Youor Groups',
+                    style: context.theme.textTheme.titleMedium?.copyWith(
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Divider(color: Colors.grey.shade300),
+                  ...yourGroups.map(YourGroupTile.new),
+                ],
+                if (otherGroups.isNotEmpty) ...[
+                  Text(
+                    'Groups',
+                    style: context.theme.textTheme.titleMedium?.copyWith(
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Divider(color: Colors.grey.shade300),
+                  ...otherGroups.map(OtherGroupTile.new),
+                ],
+              ],
+            );
           }
-          return Container();
+          return const SizedBox.shrink();
         },
       ),
     );
